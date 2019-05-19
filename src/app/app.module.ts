962e8fa1d782a8f1d1b431f9a0fff2b6fa7b5caa
mat <me@me.pl>
2019-05-19 14:13:00 +0200

    import bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenuBarComponent
   ],
   imports: [
-    BrowserModule
+    [BrowserModule, NgbModule.forRoot()],
   ],
   providers: [],
   bootstrap: [AppComponent]
